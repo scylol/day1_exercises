3bfff3dc926aa22e35099ee9b0fefab5fc7e02e4
Scylol <djd3dobbin@gmail.com>
2017-04-03 14:24:47 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// Challenge Jedi name
+
+// function jediName(firstName, lastName) {
+// 	console.log(lastName[0] + lastName[1] + lastName[2] + firstName[0] + firstName[1]);
+// }
+
+
+// jediName('Beyonce', 'Knowles');
+
+//--------------------------------------------------------------------
