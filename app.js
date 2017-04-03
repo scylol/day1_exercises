53ff347bac6dff9e5eb19b8520c39d6aff647265
Scylol <djd3dobbin@gmail.com>
2017-04-03 15:08:02 -0400

    added array

--- app.js
+++ app.js
@@ -11,19 +11,48 @@
 
 // Challenge Beyond
 
-function beyond(num){
-	if(num === Infinity){
-		console.log ("And beyond");
-	} 
-	else if( num >= 1) {
-		console.log("To infinity");
-	}
-	else if (num < 0) {
-		console.log("To negative infinity");
-	}
-	else if (num === 0) {
-		console.log("Staying home");
+// function beyond(num){
+// 	if(num === Infinity){
+// 		console.log ("And beyond");
+// 	} 
+// 	else if( num >= 1) {
+// 		console.log("To infinity");
+// 	}
+// 	else if (num < 0) {
+// 		console.log("To negative infinity");
+// 	}
+// 	else if (num === 0) {
+// 		console.log("Staying home");
+// 	}
+// }
+
+// beyond(Infinity);
+
+//---------------------------------------------------------------------
+
+//Cracking the code
+	
+function decode(string) {
+let result_array = string.split(" ");
+	for(i=0; i<string.length;i++) {
+		if(string[0] === 'a') {
+		result += string[1];
+		}
+		else if(string[0] === 'b') {
+		result += string[2];
+		}
+		else if(string[0] === 'c') {
+		result += string[3];
+		}
+		else if(string[0] === 'd') {
+		result += string[4];
+		}
+		else {
+		result += " ";
+		}
+		console.log(result);
 	}
 }
+	
 
-beyond(Infinity);
+decode('craft block argon meter bells brown croon droop');
