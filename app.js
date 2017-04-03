061cb0396df10f9dd9aca4c92187c309d23f8d1c
tachyla <tachylad.murray@gmail.com>
2017-04-03 11:36:10 -0700

    created beyond function

--- app.js
+++ app.js
@@ -8,3 +8,26 @@
 // jediName('Beyonce', 'Knowles');
 
 //--------------------------------------------------------------------
+
+
+If num is infinite (either positive or negative) it should log out 'And beyond'.
+If num is finite and positive it should log out 'To infinity'.
+If num is finite and negative it should log out 'To negative infinity'.
+If num is zero it should log out 'Staying home'.
+
+function beyond(num){
+	if(num === Infinity){
+		console.log ("And beyond");
+	} 
+	else if( num >= 1) {
+		console.log("To infinity");
+	}
+	else if (num < 0) {
+		console.log("To negative infinity");
+	}
+	else if (num === 0) {
+		console.log("Staying home");
+	}
+}
+
+beyond(1);
