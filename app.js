934e331ea235500e3218838a2e0c161fea53fe12
tachyla <tachylad.murray@gmail.com>
2017-04-03 13:36:38 -0700

    created switch case function

--- app.js
+++ app.js
@@ -32,29 +32,108 @@
 
 //Cracking the code
 	
-function decode(string) {
-	let result = '';
+// function decode(string) {
+// 	let result = '';
 
-	for(i=0; i<string.length;i+=6) {
-		if(string[i] === 'a') {
-		result += string[i+1];
-		}
-		else if(string[i] === 'b') {
-		result += string[i+2];
-		}
-		else if(string[i] === 'c') {
-		result += string[i+3];
-		}
-		else if(string[i] === 'd') {
-		result += string[i+4];
-		}
-		else {
-		result += " ";
-		}
+// 	for(i=0; i<string.length;i+=6) {
+// 		if(string[i] === 'a') {
+// 		result += string[i+1];
+// 		}
+// 		else if(string[i] === 'b') {
+// 		result += string[i+2];
+// 		}
+// 		else if(string[i] === 'c') {
+// 		result += string[i+3];
+// 		}
+// 		else if(string[i] === 'd') {
+// 		result += string[i+4];
+// 		}
+// 		else {
+// 		result += " ";
+// 		}
 
-		console.log(result);
+// 		console.log(result);
+// 	}
+// }
+
+
+// decode('craft block argon meter bells brown croon droop');
+
+
+//How Many Days
+
+function dayGenerator(month){
+	switch (month) {
+		case "January":
+		case "March":
+		case "May": 
+		case "July":
+		case "August":
+		case "October":
+		case "December":
+			console.log(`${month} has 31 days.`);
+			break;
+		
+		case "June":
+		case "November":
+		case "April":
+		case "September":
+			console.log(`${month} has 30 days.`);
+			break;		
+		
+		case "February":
+			console.log(`${month} has 28 days.`);
+			break;
+
+		default:
+			console.log("Please enter a valid month.");
+			break;
 	}
 }
 
 
-decode('craft block argon meter bells brown croon droop');
+
+dayGenerator("Cookies");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
